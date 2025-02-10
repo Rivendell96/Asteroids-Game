76be646b00ea042a846ed5908617c464617793d0
Rivendell96 <dima.tyrin@gmail.com>
2025-02-10 21:11:49 +0200

    add collections updatable and drewable

--- main.py
+++ main.py
@@ -6,23 +6,33 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    Player.containers = (updatable, drawable)
 
     while True:  
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("Black")
-        player.draw(screen)
+
+        #for obj in drawable:
+        drawable.draw(screen)
+
         pygame.display.flip()
+
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
